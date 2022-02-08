a63d4ea43f056aea6a4099680d9daa2a16b40a26
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2022-02-07 21:13:21 -0800

    Added About

---
+++ src/components/About/About.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './About.css';
+
+const About = () => {
+  return (
+    <main>
+      <div className="about-container">
+        <h1>About SFPOPOS</h1>
+        <p>POPOS are publicly accessible spaces in
+          forms of plazas, terraces, atriums, small
+          parks, and even snippets which are provided
+          and maintained by private developers. In San
+          Francisco, POPOS mostly appear in the Downtown
+          office district area.</p>
+      </div>
+    </main>
+  );
+}
+
+export default About;
